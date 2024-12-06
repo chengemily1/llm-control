5e294806a6de2c9febf719c04d0fec3bc67d19b8
chengemily1 <126461610+chengemily1@users.noreply.github.com>
2024-12-06 18:28:59 +0100

    Update save_reps.py

--- src/save_reps.py
+++ src/save_reps.py
@@ -23,7 +23,7 @@ parser.add_argument('--experiment', default='formality')
 args = parser.parse_args()
 print(args)
 
-ACCESS_TOKEN='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'
+ACCESS_TOKEN='YOUR TOKEN'
 
 # Load the model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(args.model_name, 
